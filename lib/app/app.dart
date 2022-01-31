c4a4e63719796b2179b59388884eb78e859062f7
Nobi1202 <saobang001111@gmail.com>
2022-01-31 21:06:28 +0700

    update

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,12 +1,12 @@
+import 'package:complete_advanced_flutter/presentation/resources/routes_manager.dart';
 import 'package:complete_advanced_flutter/presentation/resources/theme_manager.dart';
 import 'package:flutter/material.dart';
-
 
 class MyApp extends StatefulWidget {
   MyApp._internal(); // private named constructor
   int appState = 0;
   static final MyApp instance =
-  MyApp._internal(); // single instance -- singleton
+      MyApp._internal(); // single instance -- singleton
 
   factory MyApp() => instance; // factory for the class instance
 
@@ -18,6 +18,9 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      onGenerateRoute: RouteGenerator.getRoute,
+      initialRoute: Routes.splashRoute,
       theme: getApplicationTheme(),
     );
   }
